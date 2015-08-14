7982940289eb9ffdb7e0643fde9027289c155488
Ronan Guilbault <ronan.guilbault@gmail.com>
2015-08-14 13:21:50 +0200

    Add ranking.Team UI test
    Signed-off-by: Ronan Guilbault <ronan.guilbault@gmail.com>

---
+++ test/ui/com/bunlang/mayot/ranking/TeamUITest.java
@@ -0,0 +1,60 @@
+/*
+ * MaYoT : Manage Your Tournament
+ * Copyright (C) 2015 - Ronan GUILBAULT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.bunlang.mayot.ranking;
+
+import com.bunlang.mayot.scores.Match;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class TeamUITest {
+
+    @BeforeClass
+    public static void setupAll() {
+        // Setup L&F in UI Test
+        com.bunlang.mayot.Main.initLAF();
+    }
+
+    @Test
+    public void test() {
+        // ranking
+        Team team = new Team("Hello", 0,0,0,0,0, 0,0);
+        JPanel tUI = team.getPanel();
+        JFrame win = new JFrame();
+            win.setContentPane(tUI);
+            win.setTitle("MaYoT - org.bunlang.mayot.ranking.Team UI Test");
+            win.setSize(tUI.getSize());
+            win.setMinimumSize(tUI.getSize());
+            win.setLocationRelativeTo(null);
+        win.setVisible(true);
+
+        while(win.isShowing()) {
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        assertTrue(true);
+    }
+}
