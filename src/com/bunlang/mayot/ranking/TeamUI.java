38958abbe056dcd7eae5c643fe3544e98832fb4e
Ronan Guilbault <ronan.guilbault@gmail.com>
2015-08-14 14:02:20 +0200

    Improve ranking.Team UI
    Signed-off-by: Ronan Guilbault <ronan.guilbault@gmail.com>

--- src/com/bunlang/mayot/ranking/TeamUI.java
+++ src/com/bunlang/mayot/ranking/TeamUI.java
@@ -21,7 +21,10 @@ package com.bunlang.mayot.ranking;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 /** The view of a Team.
  *
@@ -46,38 +49,161 @@ public class TeamUI {
     protected JLabel _sFor;
     protected JLabel _sAgnst;
 
+    protected JLabel _sep1;
+    protected JLabel _sep2;
+    protected JLabel _sep3;
+
     protected JPanel _pan;
 
 
     public TeamUI(Team data) {
-        Dimension dimTtl = new Dimension(400, 20);
+        Dimension dimTtl = new Dimension(350, 16);
+        Dimension dimName = new Dimension(60, 16);
+        Dimension dimInt = new Dimension(25, 16);
+        Dimension dimSep = new Dimension(2, 16);
+        Color grey15 = new Color(0.15f, 0.15f, 0.15f);
+        Font normFont = new Font("Sansation", Font.BOLD, 12);
+
 
         _data = data;
 
         _pos = new JLabel();
+        _pos.setMinimumSize(dimInt);
+        _pos.setPreferredSize(dimInt);
+        _pos.setMaximumSize(dimInt);
+        _pos.setFont(normFont);
+        _pos.setForeground(Color.WHITE);
+        _pos.setHorizontalAlignment(SwingConstants.CENTER);
+
         _name = new JLabel();
+        _name.setMinimumSize(dimName);
+        _name.setPreferredSize(dimName);
+        _name.setMaximumSize(dimName);
+        _name.setFont(normFont);
+        _name.setForeground(Color.WHITE);
+        _name.setHorizontalAlignment(SwingConstants.CENTER);
+
         _pTotal = new JLabel();
+        _pTotal.setMinimumSize(dimInt);
+        _pTotal.setPreferredSize(dimInt);
+        _pTotal.setMaximumSize(dimInt);
+        _pTotal.setFont(normFont);
+        _pTotal.setForeground(Color.WHITE);
+        _pTotal.setHorizontalAlignment(SwingConstants.CENTER);
+
         _mPlayed = new JLabel();
+        _mPlayed.setMinimumSize(dimInt);
+        _mPlayed.setPreferredSize(dimInt);
+        _mPlayed.setMaximumSize(dimInt);
+        _mPlayed.setFont(normFont);
+        _mPlayed.setForeground(Color.WHITE);
+        _mPlayed.setHorizontalAlignment(SwingConstants.CENTER);
+
         _mWins = new JLabel();
+        _mWins.setMinimumSize(dimInt);
+        _mWins.setPreferredSize(dimInt);
+        _mWins.setMaximumSize(dimInt);
+        _mWins.setFont(normFont);
+        _mWins.setForeground(Color.WHITE);
+        _mWins.setHorizontalAlignment(SwingConstants.CENTER);
+
         _mDraws = new JLabel();
+        _mDraws.setMinimumSize(dimInt);
+        _mDraws.setPreferredSize(dimInt);
+        _mDraws.setMaximumSize(dimInt);
+        _mDraws.setFont(normFont);
+        _mDraws.setForeground(Color.WHITE);
+        _mDraws.setHorizontalAlignment(SwingConstants.CENTER);
+
         _mLoses = new JLabel();
+        _mLoses.setMinimumSize(dimInt);
+        _mLoses.setPreferredSize(dimInt);
+        _mLoses.setMaximumSize(dimInt);
+        _mLoses.setFont(normFont);
+        _mLoses.setForeground(Color.WHITE);
+        _mLoses.setHorizontalAlignment(SwingConstants.CENTER);
+
         _pBonus = new JLabel();
+        _pBonus.setMinimumSize(dimInt);
+        _pBonus.setPreferredSize(dimInt);
+        _pBonus.setMaximumSize(dimInt);
+        _pBonus.setFont(normFont);
+        _pBonus.setForeground(Color.WHITE);
+        _pBonus.setHorizontalAlignment(SwingConstants.CENTER);
+
         _pFixer = new JLabel();
+        _pFixer.setMinimumSize(dimInt);
+        _pFixer.setPreferredSize(dimInt);
+        _pFixer.setMaximumSize(dimInt);
+        _pFixer.setFont(normFont);
+        _pFixer.setForeground(Color.WHITE);
+        _pFixer.setHorizontalAlignment(SwingConstants.CENTER);
+
         _sFor = new JLabel();
+        _sFor.setMinimumSize(dimInt);
+        _sFor.setPreferredSize(dimInt);
+        _sFor.setMaximumSize(dimInt);
+        _sFor.setFont(normFont);
+        _sFor.setForeground(Color.WHITE);
+        _sFor.setHorizontalAlignment(SwingConstants.CENTER);
+
         _sAgnst = new JLabel();
+        _sAgnst.setMinimumSize(dimInt);
+        _sAgnst.setPreferredSize(dimInt);
+        _sAgnst.setMaximumSize(dimInt);
+        _sAgnst.setFont(normFont);
+        _sAgnst.setForeground(Color.WHITE);
+        _sAgnst.setHorizontalAlignment(SwingConstants.CENTER);
+
         _sDiff = new JLabel();
+        _sDiff.setMinimumSize(dimInt);
+        _sDiff.setPreferredSize(dimInt);
+        _sDiff.setMaximumSize(dimInt);
+        _sDiff.setFont(normFont);
+        _sDiff.setForeground(Color.WHITE);
+        _sDiff.setHorizontalAlignment(SwingConstants.CENTER);
+
+        _sep1 = new JLabel();
+        _sep1.setMinimumSize(dimSep);
+        _sep1.setPreferredSize(dimSep);
+        _sep1.setMaximumSize(dimSep);
+        _sep1.setBackground(Color.WHITE);
+        _sep1.setOpaque(true);
+
+        _sep2 = new JLabel();
+        _sep2.setMinimumSize(dimSep);
+        _sep2.setPreferredSize(dimSep);
+        _sep2.setMaximumSize(dimSep);
+        _sep2.setBackground(Color.WHITE);
+        _sep2.setOpaque(true);
+
+        _sep3 = new JLabel();
+        _sep3.setMinimumSize(dimSep);
+        _sep3.setPreferredSize(dimSep);
+        _sep3.setMaximumSize(dimSep);
+        _sep3.setBackground(Color.WHITE);
+        _sep3.setOpaque(true);
 
         _pan = new JPanel();
         _pan.setLayout(new BoxLayout(_pan, BoxLayout.LINE_AXIS));
         _pan.add(_pos);
+
+        _pan.add(_sep1);
+
         _pan.add(_name);
         _pan.add(_pTotal);
+
+        _pan.add(_sep2);
+
         _pan.add(_mPlayed);
         _pan.add(_mWins);
         _pan.add(_mDraws);
         _pan.add(_mLoses);
         _pan.add(_pBonus);
         _pan.add(_pFixer);
+
+        _pan.add(_sep3);
+
         _pan.add(_sFor);
         _pan.add(_sAgnst);
         _pan.add(_sDiff);
@@ -86,6 +212,7 @@ public class TeamUI {
         _pan.setPreferredSize(dimTtl);
         _pan.setMaximumSize(dimTtl);
         _pan.setSize(dimTtl);
+        _pan.setBackground(grey15);
 
         this.update();
     }
